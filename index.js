5035579f96fc8fc94c0052e59a69774cdb03a753
Benjamin <bghassemi@live.nl>
2021-08-18 22:03:28 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function text() {
+    document.getElementById("containerOne").innerText = "My name is Benjamin Ghassemi, I'm 30 years old and have been living in Amsterdam for 7 years. "
+
+}
+function text2() {
+    document.getElementById("containerTwo").innerText = "My hobbys are gaming(ofcource), clubbing, traveling, footbal and hanging out with friends."
+}
+
+function text3() {
+    document.getElementById("containerThree").innerText = "I was born in Iran and moved to the Netherlands with my parents in 1994, I love late night snacks and retro games. Somewhere in the future I hope to shift my passion for coding to building videogames. But for now I'm gonna focus all my eforts to ace this bootcamp!"
+}
